ac32e96dbe1b58ce2d917b989ca970c4b421572b
ishaarora01 <ishaarora.87@gmail.com>
2019-11-25 21:10:57 -0500

    jenkins

---
+++ src/test/java/com/example/usersapi/service/UserServiceTest.java
@@ -0,0 +1,33 @@
+package com.example.usersapi.service;
+
+import com.example.usersapi.model.User;
+import com.example.usersapi.repository.UserRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserServiceTest {
+
+    @Mock
+    UserRepository userRepository;
+
+    @InjectMocks
+    UserServiceImpl userService;
+
+    @Test
+    public void test(){
+        User user = new User();
+
+        when(userRepository.save(any())).thenReturn(user);
+        userService.createUser(user);
+        Assert.assertEquals("hi","hi");
+    }
+}
